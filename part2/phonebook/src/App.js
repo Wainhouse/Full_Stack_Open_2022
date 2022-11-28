b9e26453f850e83c65f2036013c561b5df92c5eb
Luke Wainhouse <lukewainhouse@hotmail.com>
2022-11-28 15:43:50 +0000

    LW: 2.10 completed, components

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,12 +1,19 @@
 import { useState } from "react";
 import Persons from "./components/Persons";
+import { Form } from "./components/Form";
+import { Search } from "./components/Search";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
-  const [filterName, setFilterName] = useState("")
-  const [personsSearch, setPersonsSearch] = useState(persons)
+  const [filterName, setFilterName] = useState("");
+  const [personsSearch, setPersonsSearch] = useState(persons);
 
   const handlePersonChange = (event) => {
     console.log(event.target.value);
@@ -45,17 +52,16 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <p>Filter shown with : <input value={filterName} onChange={handleSearch} /></p>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handlePersonChange} />
-          <br/>
-          number: <input value={newNumber} onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <Search
+      filterName={filterName}
+      handleSearch={handleSearch}/>
+      <Form
+      addPerson = {addPerson}
+      newName = {newName}
+      newNumber = {newNumber}
+      handlePersonChange = {handlePersonChange}
+      handleNumberChange = {handleNumberChange}
+      />
       <h2>Numbers</h2>
       {personsSearch.map((person) => (
         <Persons key={person.name} person={person} number={newNumber}/>
---
+++ part2/phonebook/src/components/Search.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Search = ({filterName, handleSearch}) => {
+  return (
+<p>
+    Filter shown with : <input value={filterName} onChange={handleSearch} />
+</p>  )
+}
