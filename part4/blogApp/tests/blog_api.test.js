3143e1b9c053aa75bb5295dcfd34cc2a6f2d6064
Luke Wainhouse <lukewainhouse@hotmail.com>
2023-01-24 11:19:01 +0000

    LW: Also added 4.14 Blog list expansions, step2

--- part4/blogApp/tests/blog_api.test.js
+++ part4/blogApp/tests/blog_api.test.js
@@ -15,7 +15,7 @@ beforeEach(async () => {
   await Promise.all(promiseArray);
 });
 
-describe('when there is initially some blogs saved', () => {
+describe('when there is some blogs saved', () => {
   test('blogs are returned as json', async () => {
     await api
       .get('/api/blogs')
